8a6e3bba1238eb6b0c77556d02bb2ef5203854f3
ana <ana.g.kostanjevec@gmail.com>
2023-09-25 09:41:56 +0200

    Added catkin_ws and my_first_node.py

---
+++ catkin_ws/build/test1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/FEROS2023-dan0/FEROS_1/catkin_ws/src/test1
+# Build directory: /home/student/FEROS2023-dan0/FEROS_1/catkin_ws/build/test1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
